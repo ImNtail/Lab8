ba96e8f52dc7ff1d6ba6cee80626250ccfd25e11
Nikita Maslov <55882052+ImNtail@users.noreply.github.com>
2020-04-13 17:40:22 +0300

    Update first.cs

--- Lab8/Lab8/first.cs
+++ Lab8/Lab8/first.cs
@@ -41,7 +41,7 @@ namespace Lab8
             string indexOfNumber = String.Empty;
             int length = 100000;
             int[] array = new int[length];
-            using (StreamReader readArray = new StreamReader(new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sorted.dat"), FileMode.OpenOrCreate)))
+            using (StreamReader readArray = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sorted.dat")))
             {
                 try
                 {
